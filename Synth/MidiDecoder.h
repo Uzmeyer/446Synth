63ff547f2987415e763795850bcf2daa14ae5c2b
Uzmeyer <jakob.goessinger@gmail.com>
2019-05-23 00:14:08 +0200

    Update MidiDecoder.h

--- Synth/MidiDecoder.h
+++ Synth/MidiDecoder.h
@@ -10,6 +10,20 @@
 
 #include "Synth.h"
 
+
+
+enum MidiMessageType
+{
+	MIDI_NOTE_OFF = 0x80,
+	MIDI_NOTE_NOTEON = 0x90,
+	MIDI_NOTE_POLY_PRESSURE = 0xA0, //poly aftertouch
+	MIDI_CONTROL_CHNAGE = 0xB0,
+	MIDI_PROGRAM_CHANGE = 0xC0, //program change, only one command byte
+	MIDI_CHANNEL_PRESSURE = 0xD0, //channel aftertouch, only one command byte
+	MIDI_PITCHBEND = 0xE0,
+	MIDI_SYSTEM = 0xF0
+};
+
 namespace std {
 
 class MidiDecoder {
